f384dc10d1aa60bc35cf2d2e4208ab0cd3dc1b69
vigneshj04 <vigneshjmax@gmail.com>
2022-04-17 21:16:26 +0530

    Added: Rule-1 For Password Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -15,20 +15,30 @@ public class UserRegistration {
         String eMail = input.next();
         System.out.println("Enter the Mobile Number");
         String mobileNumber = input.next();
+        System.out.println("Enter the PassWord");
+        String passWord = input.next();
+
 
         Pattern P = Pattern.compile("^[A-Z][a-z]{2}$");
         Pattern P1 = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
         Pattern P2 = Pattern.compile("[91?]\\s[6-9][0-9]{9}$");
+        Pattern P3 = Pattern.compile("[A-z]{8}$");
+
         Matcher M = P.matcher(firstName);
         Matcher M1 = P.matcher(lastName);
         Matcher M2 = P1.matcher(eMail);
         Matcher M3 = P2.matcher(mobileNumber);
+        Matcher M4 = P3.matcher(passWord);
 
         if (M.find() && M1.find()) {
             if (M2.find() && M3.find()) {
-                System.out.println("Your Details are Valid");
+                if (M4.find()) {
+                    System.out.println("Your Details are Valid");
+                } else {
+                    System.out.println("Please Check Your PassWord");
+                }
             } else {
-                System.out.println("Please Check Your Email and Mobile Number");
+                System.out.println("Please Check Your EMail and Mobile Number");
             }
         } else {
             System.out.println("Please Check Your First Name and Last Name");

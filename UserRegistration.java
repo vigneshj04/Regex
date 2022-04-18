ac6757212484b06bbc9d1c39a269651349f63491
vigneshj04 <vigneshjmax@gmail.com>
2022-04-18 20:42:51 +0530

    Added: Sample Emails validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,47 +1,42 @@
 package com.regex;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("Enter Your First Name: ");
-        Scanner input = new Scanner(System.in);
-        String firstName = input.next();
-        System.out.println("Enter the Last Name");
-        String lastName = input.next();
-        System.out.println("Enter the E-Mail-ID");
-        String eMail = input.next();
-        System.out.println("Enter the Mobile Number");
-        String mobileNumber = input.next();
-        System.out.println("Enter the PassWord");
-        String passWord = input.next();
 
+        ArrayList emails = new ArrayList();
+        emails.add("abc@yahoo.com");
+        emails.add("abc-100@yahoo.com");
+        emails.add("abc.100@yahoo.com");
+        emails.add("abc111@abc.com");
+        emails.add("abc-100@abc.net");
+        emails.add("abc.100@abc.com.au");
+        emails.add("abc@1.com");
+        emails.add("abc+100@gmail.com");
 
-        Pattern P = Pattern.compile("^[A-Z][a-z]{2}$");
-        Pattern P1 = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
-        Pattern P2 = Pattern.compile("[91?]\\s[6-9][0-9]{9}$");
-        Pattern P3 = Pattern.compile("^[A-Za-z]{6}[@$!%*?&][0-9]$");
+        emails.add("abc@gmail.com.aa.au");
+        emails.add("abc@gmail.com.1a");
+        emails.add("abc@abc@gmail.com");
+        emails.add("abc.@gmail.com");
+        emails.add("abc..2002@gmail.com");
+        emails.add("abc@%*.com");
+        emails.add("abc()*@gmail.com");
+        emails.add(".abc@abc.com");
+        emails.add("abc123@.com.com");
+        emails.add("abc123@.com");
+        emails.add("abc123@gmail.a");
 
-        Matcher M = P.matcher(firstName);
-        Matcher M1 = P.matcher(lastName);
-        Matcher M2 = P1.matcher(eMail);
-        Matcher M3 = P2.matcher(mobileNumber);
-        Matcher M4 = P3.matcher(passWord);
+        String regex = ("^[A-Za-z0-9+_.-]@(.+)$");
 
-        if (M.find() && M1.find()) {
-            if (M2.find() && M3.find()) {
-                if (M4.find()) {
-                    System.out.println("Your Details are Valid");
-                } else {
-                    System.out.println("Please Check Your PassWord");
-                }
-            } else {
-                System.out.println("Please Check Your EMail and Mobile Number");
-            }
-        } else {
-            System.out.println("Please Check Your First Name and Last Name");
+        Pattern pattern = Pattern.compile(regex);
+
+        for(Object email : emails){
+            Matcher matcher = pattern.matcher((CharSequence) email);
+            System.out.println(email +" : "+ matcher.matches());
+        }
         }
     }
-}
